9dfb0db1bfc2eb4ab37691570460537c01ee24ad
which1ispink <arsenalwa@hotmail.com>
2016-04-25 23:50:00 +0200

    Clean some code up and add the ability to change the time interval with up and down arrow keys

--- js/game_engine.js
+++ js/game_engine.js
@@ -1,16 +1,15 @@
 var GRID_SIZE = 10;
-var GRID_DIV_ID = 'grid';
-var GRID_UPDATE_INTERVAL = 1000;
+var UPDATE_TIMEOUT = 1000;
 var CELL_DIV_CLASS = 'cell';
 var cell_colors = {
 	0: '#A30000',
 	1: '#0404AD',
-	2: '#0b7913'
+	2: '#0f6b16'
 };
 
 function init() {
 	generate_grid();
-	setInterval(regenerate_cells_colors, GRID_UPDATE_INTERVAL);
+	setTimeout(regenerate_cells_colors, UPDATE_TIMEOUT);
 }
 
 function generate_grid() {
@@ -18,7 +17,7 @@ function generate_grid() {
 		for (var col = 0; col < GRID_SIZE; col++) {
 			var random_color = pick_random_color_for_cell();
 			var cell = generate_cell('tile_' + row + 'x' + col, random_color);
-			$('#' + GRID_DIV_ID).append(cell);
+			$('#grid').append(cell);
 		}
 	}
 }
@@ -29,8 +28,12 @@ function generate_cell(cell_id, cell_background_color) {
 	return cell;
 }
 
-function pick_random_element_from_array(array) {
-	return array[Math.floor(Math.random() * array.length)];
+function regenerate_cells_colors() {
+	$('.' + CELL_DIV_CLASS).each(function(i) {
+		var random_color = pick_random_color_for_cell();
+	    $(this).css('background-color', random_color);
+	});
+	setTimeout(regenerate_cells_colors, UPDATE_TIMEOUT);
 }
 
 function pick_random_color_for_cell() {
@@ -38,11 +41,25 @@ function pick_random_color_for_cell() {
 	return cell_colors[random_color_idx];
 }
 
-function regenerate_cells_colors() {
-	$('.' + CELL_DIV_CLASS).each(function(i) {
-		var random_color = pick_random_color_for_cell();
-	    $(this).css('background-color', random_color);
-	});
+function pick_random_element_from_array(array) {
+	return array[Math.floor(Math.random() * array.length)];
 }
 
-window.addEventListener("load", init);
+// window.addEventListener("load", init);
+$(document).ready(function() {
+	init();
+});
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 38: // up
+			UPDATE_TIMEOUT -= 100;
+        	break;
+        case 40: // down
+			UPDATE_TIMEOUT += 100;
+        	break;
+        default:
+			return;
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
